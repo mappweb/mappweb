c8fa779b1b16e4705f27b4ac3a322649795573fd
Diego <diego.toscanof@gmail.com>
2019-08-30 13:38:58 -0500

    updated Directory Class

--- src/Helpers/Directory.php
+++ src/Helpers/Directory.php
@@ -12,6 +12,20 @@ use File;
 
 class Directory
 {
+
+    protected $path;
+    protected $file;
+    /**
+     * Directory constructor.
+     * @param $uuid
+     * @param null $file
+     * @param string $type
+     */
+    public function __construct($uuid, $file = null, $type = 'files')
+    {
+        $this->path = $type . '/' . substr($uuid, 0, 2) . '/' . substr($uuid, 2, 2) . '/' . substr($uuid, 4) . '/';
+        $this->file = $file;
+    }
     /**
      * Create directories if not exist
      *
@@ -21,5 +35,21 @@ class Directory
         if(!File::exists($path)) {
             File::makeDirectory($path, $mode = 0777, true, true);
         }
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return "{$this->path}";
+    }
+
+    /**
+     * @return string
+     */
+    public function getFilePath()
+    {
+        return "{$this->getPath()}"."{$this->file}";
     }
 }
